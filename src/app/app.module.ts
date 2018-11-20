72a93ff500187515ba3b8dfc4994cd0446f5bd7d
Filip Talaga <talaga.filip@gmail.com>
2018-11-20 19:53:05 +0100

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+/* Modules */
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+/* Entry */
 import { AppComponent } from './app.component';
 
+/* Custom modules */
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+
+/* Pages */
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        CommonModule,
+        FormsModule,
+        AngularMaterialModule,
+        ReactiveFormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
